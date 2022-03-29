bd9a5da658972603f904d63242ec28945938012a
Badiaa Bouhdid <badiaabouhdid@gmail.com>
2022-03-29 21:28:40 +0200

    code seance 4: Interpolation + Property Binding

--- src/app/list-product/list-product.component.ts
+++ src/app/list-product/list-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from "../model/product";
 
 @Component({
   selector: 'app-list-product',
@@ -7,11 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListProductComponent implements OnInit {
   public titleApp: string;
-  public  name: string;
+  public listProduct:Product[];
   constructor() {
   }
   ngOnInit(): void {
     this.titleApp= 'First Angular App';
+    this.listProduct= [
+      { id: 12,
+        title: 'T-shirt 1',
+        price: 120,
+        quantity: 0,
+        picture: 'https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F49%2F56%2F49560dfa45732ed844e8e4742afdca248ee60f9f.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_tshirtstanks_shortsleeve%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]',
+        like: 12,
+        description: 'test description'},
+      { id: 123,
+        title: 'T-shirt 2',
+        price: 10,
+        quantity: 10,
+        picture: 'https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F49%2F56%2F49560dfa45732ed844e8e4742afdca248ee60f9f.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_tshirtstanks_shortsleeve%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]',
+        like: 12,
+        description: 'test description'},
+      { id: 34,
+        title: 'T-shirt 3',
+        price: 10,
+        quantity: 10,
+        picture: 'https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F49%2F56%2F49560dfa45732ed844e8e4742afdca248ee60f9f.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_tshirtstanks_shortsleeve%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]',
+        like: 12,
+        description: 'test description'}
+    ]
   }
 
 
---
+++ src/app/model/product.ts
@@ -0,0 +1,9 @@
+export class Product{
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  quantity: number;
+  picture: string;
+  like: number;
+}
